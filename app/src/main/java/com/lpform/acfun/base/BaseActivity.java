1f1c0ef902ddd11b03c04db929494da27e905195
ylsddlp <1151449905@qq.com>
2020-01-15 14:25:22 +0800

    Initial commit

---
+++ app/src/main/java/com/lpform/acfun/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.lpform.acfun.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @Auther: lipeng
+ * @Date: 2020/1/15/0015 11:28
+ * @Description:
+ */
+public class BaseActivity extends AppCompatActivity {
+    public String TAG =getClass().getSimpleName();
+
+
+}
---
+++ app/src/main/java/com/lpform/acfun/controller/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lpform.acfun.controller.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.lpform.acfun.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
